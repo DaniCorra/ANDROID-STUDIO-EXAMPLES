427edca2bc6c93522d80472697ca2ac425908055
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-03 07:41:50 +0100

    Adding thread

---
+++ ThreadApp/app/src/main/java/com/kocolondon/threadapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.kocolondon.threadapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+
+public class MainActivity extends Activity {
+
+    private Handler myHandler;
+    boolean gameOn;
+    long startTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startTime = System.currentTimeMillis();
+
+        myHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if(gameOn) {
+                    long seconds = ((System.currentTimeMillis() - startTime)) / 1000;
+                    Log.i("info", "seconds = " + seconds);
+                }
+                myHandler.sendEmptyMessageDelayed(0, 1000);
+            }
+        };
+        gameOn = true;
+        myHandler.sendEmptyMessage(0);
+    }
+
+}
