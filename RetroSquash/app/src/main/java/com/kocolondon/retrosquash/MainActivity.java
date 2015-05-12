d76c337dafee5492366e6ed8dbb55208ab00256c
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-12 15:57:12 +0100

    Define local vars

--- RetroSquash/app/src/main/java/com/kocolondon/retrosquash/MainActivity.java
+++ RetroSquash/app/src/main/java/com/kocolondon/retrosquash/MainActivity.java
@@ -2,9 +2,66 @@ package com.kocolondon.retrosquash;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import java.io.IOException;
+import java.util.Random;
 
+public class MainActivity extends Activity {
+    Canvas canvas;
+    SquashCourtView squashCourtView;
 
-public class MainActivity extends ActionBarActivity {
+    //Sound
+    //initialize sound variables
+    private SoundPool soundPool;
+    int sample1 = -1;
+    int sample2 = -1;
+    int sample3 = -1;
+    int sample4 = -1;
+
+    //For getting display details like the number of pixels
+    Display display;
+    Point size;
+    int screenWidth;
+    int screenHeight;
+
+    //Game objects
+    int racketWidth;
+    int racketHeight;
+    Point racketPosition;
+
+    Point ballPosition;
+    int ballWidth;
+
+    //for ball movement
+    boolean ballIsMovingLeft;
+    boolean ballIsMovingRight;
+    boolean ballIsMovingUp;
+    boolean ballIsMovingDown;
+
+    //for racket movement
+    boolean racketIsMovingLeft;
+    boolean racketIsMovingRight;
+
+    //stats
+    long lastFrameTime;
+    int fps;
+    int score;
+    int lives;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
