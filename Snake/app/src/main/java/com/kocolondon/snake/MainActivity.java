5b1cf0c4400c4148de644f4e1ff773dda300fa74
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-18 20:34:02 +0100

    Adding SnakeAnimView class

--- Snake/app/src/main/java/com/kocolondon/snake/MainActivity.java
+++ Snake/app/src/main/java/com/kocolondon/snake/MainActivity.java
@@ -1,38 +1,98 @@
 package com.kocolondon.snake;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    Canvas canvas;
+    SnakeAnimView snakeAnimView;
+
+    //The snake head sprite sheet
+    Bitmap headAnimBitmap;
+
+    //The portion of the bitmap to be drawn in the current frame
+    Rect rectToBeDrawn;
+
+    //The dimensions of a single frame
+    int frameHeight = 64;
+    int frameWidth = 64;
+    int numFrames = 6;
+    int frameNumber;
+    int screenWidth;
+    int screenHeight;
+
+    //stats
+    long lastFrameTime;
+    int fps;
+    int hi;
+    //To start the game from onTouchEvent
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        //find out the width and height of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        headAnimBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.head_sprite_sheet);
+        snakeAnimView = new SnakeAnimView(this);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        setContentView(snakeAnimView);
+        i = new Intent(this, GameActivity.class);
+
+        class SnakeAnimView extends SurfaceView implements Runnable {
+            Thread ourThread = null;
+            SurfaceHolder ourHolder;
+            volatile boolean playingSnake;
+            Paint paint;
+
+            public SnakeAnimView(Context context) {
+                super(context);
+                ourHolder = getHolder();
+                paint = new Paint();
+                frameWidth = headAnimBitmap.getWidth()/numFrames;
+                frameHeight = headAnimBitmap.getHeight();
+            }
+
+            @Override
+            public void run() {
+                while (playingSnake) {
+                    update();
+                    draw();
+                    controlFPS();
+                }
+            }
+
+            public void update() {
+                //which frame should we draw
+                rectToBeDrawn = new Rect((frameNumber * frameWidth)-1,
+                        0,(frameNumber * frameWidth +frameWidth)-1,
+                        frameHeight);
+                //now the next frame
+                frameNumber++;
+                //don't try and draw frames that don't exist
+                if(frameNumber == numFrames){
+                    frameNumber = 0;//back to the first frame
+                }
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
