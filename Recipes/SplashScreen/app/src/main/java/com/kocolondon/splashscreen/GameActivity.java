6b4c85cf0a5a1c90c2aa8705109f573fdb84160a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-28 07:24:48 +0100

    Adding splash green

---
+++ Recipes/SplashScreen/app/src/main/java/com/kocolondon/splashscreen/GameActivity.java
@@ -0,0 +1,14 @@
+package com.kocolondon.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
---
+++ Recipes/SplashScreen/app/src/main/java/com/kocolondon/splashscreen/MainActivity.java
@@ -0,0 +1,32 @@
+package com.kocolondon.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends Activity {
+
+    // 10 seconds
+    static int GAME_THREAD_DELAY = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
+                MainActivity.this.startActivity(gameActivity);
+
+                // perform all house keeping activities here
+
+                GAME_THREAD_DELAY = 1000;
+                
+                MainActivity.this.finish();
+            }
+        }, GAME_THREAD_DELAY);
+    }
+}
